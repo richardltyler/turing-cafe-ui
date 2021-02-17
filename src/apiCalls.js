06d2474ad1d83a9a6917ecd99d3ad8a05d07330c
Richard Tyler <richardtyler2011@gmail.com>
2021-02-17 10:10:54 -0700

    Make post function in apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,26 @@
+const baseUrl = 'http://localhost:3001/api/v1/reservations';
+
 const apiCalls = {
   getAllReservations() {
     return (
-      fetch('http://localhost:3001/api/v1/reservations')
+      fetch(baseUrl)
+    );
+  },
+
+  makeNewRes(newRes) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(newRes)
+    };
+
+    return (
+      fetch(baseUrl, options)
     );
   }
+
 }
 
 export default apiCalls;
