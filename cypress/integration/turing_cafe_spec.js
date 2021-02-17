bcfc117b13e02a2f77bb3979c5d0bc96831d2be1
Richard Tyler <richardtyler2011@gmail.com>
2021-02-17 11:09:08 -0700

    Refactor test to be more readable

--- cypress/integration/turing_cafe_spec.js
+++ cypress/integration/turing_cafe_spec.js
@@ -17,95 +17,96 @@ describe('Turing Cafe', () => {
       .contains('Turing Cafe Reservations');
   });
 
-it('should display a form', () => {
-  cy.get('form');
-})
- it('should display a form with 4 input fields', () => {
-   cy.get('form input')
-    .its('length')
-    .should('eq', 4);
- });
+  it('should display a form', () => {
+    cy.get('form');
+  });
 
- it('should have an input for name', () => {
-  cy.get('form input[name=name]');
- });
+  it('should display a form with 4 input fields', () => {
+    cy.get('form input')
+      .its('length')
+      .should('eq', 4);
+  });
 
- it('should have an input for date', () => {
-  cy.get('form input[name=date]');
- });
+  it('should have an input for name', () => {
+    cy.get('form input[name=name]');
+  });
 
- it('should have an input for time', () => {
-  cy.get('form input[name=time]')
- });
+  it('should have an input for date', () => {
+    cy.get('form input[name=date]');
+  });
 
- it('should have an input for number of guests', () => {
-  cy.get('form input[name=number]');
- });
+  it('should have an input for time', () => {
+    cy.get('form input[name=time]')
+  });
 
- it('should have a button to make a reservation', ()=> {
-   cy.get('form button');
- });
+  it('should have an input for number of guests', () => {
+    cy.get('form input[name=number]');
+  });
 
- it('should display reservation cards', () => {
-   cy.get('section')
-    .children()
-    .its('length')
-    .should('eq', 3);
- });
+  it('should have a button to make a reservation', ()=> {
+    cy.get('form button');
+  });
 
- it('should display res cards with a name', () => {
-   cy.get('article')
+  it('should display reservation cards', () => {
+    cy.get('section')
+      .children()
+      .its('length')
+      .should('eq', 3);
+  });
+
+  it('should display res cards with a name', () => {
+    cy.get('article')
     .get('h2')
     .contains('Christie');
 
   cy.get('article')
     .get('h2')
     .contains('Leta');
-  
+
   cy.get('article')
     .get('h2')
     .contains('Pam');
- });
+  });
 
- it('should display res cards with a date', () => {
+  it('should display res cards with a date', () => {
   cy.get('article')
-  .get('h3')
-  .contains('12/29');
-
-  cy.get('article')
-  .get('h3')
-  .contains('4/5');
+    .get('h3')
+    .contains('12/29');
 
   cy.get('article')
-  .get('h3')
-  .contains('1/21'); 
- });
-
- it('should display res cards with a time', () => {
-  cy.get('article')
-  .get('h4')
-  .contains('7:00pm');
+    .get('h3')
+    .contains('4/5');
 
   cy.get('article')
-  .get('h4')
-  .contains('7:00pm');
+    .get('h3')
+    .contains('1/21'); 
+  });
+
+  it('should display res cards with a time', () => {
+  cy.get('article')
+    .get('h4')
+    .contains('7:00pm');
 
   cy.get('article')
-  .get('h4')
-  .contains('7:00pm'); 
- });
-
- it('should display res cards with a number of guests', () => {
-  cy.get('article')
-  .get('h5')
-  .contains('Number of Guests: 12');
+    .get('h4')
+    .contains('7:00pm');
 
   cy.get('article')
-  .get('h5')
-  .contains('Number of Guests: 2');
+    .get('h4')
+    .contains('7:00pm'); 
+  });
+
+  it('should display res cards with a number of guests', () => {
+  cy.get('article')
+    .get('h5')
+    .contains('Number of Guests: 12');
 
   cy.get('article')
-  .get('h5')
-  .contains('Number of Guests: 4'); 
- });
+    .get('h5')
+    .contains('Number of Guests: 2');
+
+  cy.get('article')
+    .get('h5')
+    .contains('Number of Guests: 4'); 
+  });
 })
