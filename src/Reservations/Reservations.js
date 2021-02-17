bc15f59286ec2bcc5e9fee0fa44ba48cd9434566
Richard Tyler <richardtyler2011@gmail.com>
2021-02-17 09:35:44 -0700

    Display all reservations on cards

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Card from '../Card/Card';
 import './Reservations.css';
 
 const Reservations = ({ resys }) => {
+  const resyCards = resys.map(resy => {
+    return (
+      <Card 
+        id={resy.id}
+        key={resy.id}
+        name={resy.name}
+        data={resy.date}
+        time={resy.time}
+        number={resy.time}
+      />
+    )
+  })
+
   return (
     <section className='res-container'>
+      {resyCards}
     </section>
   )
 }
