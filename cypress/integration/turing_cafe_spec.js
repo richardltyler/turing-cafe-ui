e44a8b71443784763e5c9286e609fd640cef4d01
Richard Tyler <richardtyler2011@gmail.com>
2021-02-17 10:38:09 -0700

    Test Form component's display

--- cypress/integration/turing_cafe_spec.js
+++ cypress/integration/turing_cafe_spec.js
@@ -5,10 +5,39 @@ describe('Turing Cafe', () => {
     cy.visit(baseUrl);
   });
 
-  it.only('should have a headline', () => {
+  it('should have a headline', () => {
     cy.get('h1')
       .contains('Turing Cafe Reservations');
   });
+
+it('should display a form', () => {
+  cy.get('form');
+})
+ it('should display a form with 4 input fields', () => {
+   cy.get('form input')
+    .its('length')
+    .should('eq', 4);
+ });
+
+ it('should have an input for name', () => {
+  cy.get('form input[name=name]');
+ });
+
+ it('should have an input for date', () => {
+  cy.get('form input[name=date]');
+ });
+
+ it('should have an input for time', () => {
+  cy.get('form input[name=time]')
+ });
+
+ it('should have an input for number of guests', () => {
+  cy.get('form input[name=number]');
+ });
+
+ it('should have a button to make a reservation', ()=> {
+   cy.get('form button');
+ });
 
 
 })
